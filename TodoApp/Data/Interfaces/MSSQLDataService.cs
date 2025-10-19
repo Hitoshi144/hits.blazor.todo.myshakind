221efcecbf702e6c3739d7b2cab786d2ea830e4b
Hitoshi <myshakin2018@mail.ru>
2025-10-19 23:25:00 +0500

    Lecture 4

---
+++ TodoApp/Data/Interfaces/MSSQLDataService.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp.Data.Interfaces {
+    public class MSSQLDataService(ApplicationDbContext context) : IDataService {
+        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+        {
+            return await context.TaskItems.ToArrayAsync();
+        }
+    }
+}
